8c6474f673e6142d7644b5431d5068c42d29810a
git <morloragus@gmail.com>
2018-05-08 11:44:23 -0500

    se eliminan dependencias innecesarias

--- src/main/java/com/baz/example/controller/PersonaController.java
+++ src/main/java/com/baz/example/controller/PersonaController.java
@@ -2,30 +2,23 @@ package com.baz.example.controller;
 
 import java.util.List;
 
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.baz.example.bo.PersonaBO;
 import com.baz.example.dominio.Persona;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class PersonaController 
 {
 	@Autowired
 	PersonaBO personaBO;
-	private static final Logger log = Logger.getLogger(PersonaController.class.getName());
-	
-	
 	
 	@RequestMapping("/persona/list")
 	public List<Persona> list()
 	{
-		log.info(">>>Consumiendo Servicio GET LISTA PERSONAS<<<");
+		System.out.println(">>>Consumiendo Servicio GET LISTA PERSONAS<<<");
 		return personaBO.list();
 	}
 	
